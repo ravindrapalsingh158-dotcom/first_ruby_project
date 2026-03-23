f0c4efff84b27b4fe3dcc4f6c5ab54b92a2c24f6
Ravindra pal singh <ravindrapalsingh158@gmail.com>
2026-03-23 22:07:20 +0530

    update main file

--- input_output/main.rb
+++ input_output/main.rb
@@ -1,13 +1,16 @@
-puts " please enter your name"
-name= gets.chomp
 
-puts "please enter your price"
+loop do
+puts "Please enter the item name:"
+name= gets.chomp
+ break if name.empty? 
+
+puts "please enter your price :"
 price= gets.chomp.to_f
 
-puts "please enter your quantity"
+puts "please enter your quantity ::"
 quantity= gets.chomp.to_i
 
-puts "is the item imported yes/no"
+puts "is the item imported yes/no :"
  imported= gets.chomp.downcase
  if imported == "yes"
     imported = true
@@ -24,7 +27,7 @@ calculator = TaxCalculator.new(item)
 tax = calculator.total_tax
     total_price = calculator.price_with_tax
     puts "the total price for #{item.name} is #{total_price}"
-
+end
 
 
 
