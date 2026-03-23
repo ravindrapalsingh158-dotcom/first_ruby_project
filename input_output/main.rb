7cc76483df27d8cd668bd38f717e9696d36884ae
Ravindra pal singh <ravindrapalsingh158@gmail.com>
2026-03-23 16:22:59 +0530

    input file creation

---
+++ input_output/main.rb
@@ -0,0 +1,17 @@
+puts " please entre your name"
+name= gats.chomp
+
+puts "please enter your price"
+price= gets.chomp.to_f
+
+puts "plese enter your quantity"
+quantity= get.champ.to_i
+
+puts "is the item imported yes/no"
+ imported= gets.chomp.downcae
+ if imported == "yes"
+    improted = true
+ else
+    imported=false
+ end
+ 
