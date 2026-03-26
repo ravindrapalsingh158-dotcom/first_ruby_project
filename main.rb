e87821da0320cb2dedb6151cbe51f6e72373e82b
Ravindra pal singh <ravindrapalsingh158@gmail.com>
2026-03-26 21:46:05 +0530

    update in main file

--- main.rb
+++ main.rb
@@ -4,10 +4,13 @@ require_relative 'lib/tax_calculation'
 
 basket = SalesTax::Basket.new
 
+total_tax = 0
+total_price = 0
+
 loop do
   puts "Please enter the item name:"
-  name = gets.chomp
-  break if name.empty?
+name = gets.chomp
+break if name.strip.empty?
 
   puts "please enter your price :"
   price = gets.chomp.to_f
@@ -23,9 +26,14 @@ loop do
   basket.add_item(item)
 
   tax = SalesTax::TaxCalculation.calculate(item)
-total_price = (item.price + tax) * item.quantity
 
-puts "the total price for #{item.name} is #{total_price}"
+  item_total_tax = tax * item.quantity
+  item_total_price = (item.price + tax) * item.quantity
 
-
+  total_tax += item_total_tax
+  total_price += item_total_price
 end
+
+puts "----------------------"
+puts "the total sales tax is #{total_tax.round(2)}"
+puts "the total price is #{total_price.round(2)}"
